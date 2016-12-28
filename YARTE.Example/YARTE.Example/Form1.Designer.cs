8e7e47e7bf6f4c1271b82d1fed147b7573f8ce29
Peter Humbert <humbe037@umn.edu>
2016-12-28 16:37:06 -0600

    Fix scaling issue
    Text should no longer extend beyond boundary of control

--- YARTE.Example/YARTE.Example/Form1.Designer.cs
+++ YARTE.Example/YARTE.Example/Form1.Designer.cs
@@ -56,7 +56,6 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(723, 433);
             this.Controls.Add(this.button1);
