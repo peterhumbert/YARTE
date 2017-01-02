98d4c6ff5142238e46dfcc4fe406631669c7ef02
Peter Humbert <humbe037@umn.edu>
2017-01-01 21:24:25 -0600

    add CheckboxItem class
    provides a basis for externally adding checkboxes

---
+++ YARTE/YARTE/CheckboxItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YARTE
+{
+    public class CheckboxItem
+    {
+        private string strLabel;
+        private int intIdentifier;
+
+        public CheckboxItem(string label, int identifier)
+        {
+            strLabel = label;
+            intIdentifier = identifier;
+        }
+
+        public string Label
+        {
+            get { return strLabel; }
+            set { strLabel = value; }
+        }
+
+        public int Identifier
+        {
+            get { return intIdentifier; }
+            set { intIdentifier = value; }
+        }
+    }
+}
