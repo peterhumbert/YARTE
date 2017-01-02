d44a171f9a640ebc78e8ba211648193f1dec65a3
Peter Humbert <humbe037@umn.edu>
2017-01-01 22:00:44 -0600

    begin implementing external checkbox items in example
    add dialog form and its interactions with the main form

---
+++ YARTE.Example/YARTE.Example/AddTaskDialog.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace YARTE.Example
+{
+    partial class AddTaskDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(244, 75);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(325, 75);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(388, 22);
+            this.txtInput.TabIndex = 2;
+            // 
+            // AddTaskDialog
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(412, 110);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCancel);
+            this.Name = "AddTaskDialog";
+            this.Text = "AddTaskDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
--- YARTE.Example/YARTE.Example/Form1.cs
+++ YARTE.Example/YARTE.Example/Form1.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 using YARTE.UI.Buttons;
+using System;
 
 namespace YARTE.Example
 {
     public partial class Form1 : Form
     {
+        private List<CheckboxItem> tasks = new List<CheckboxItem>();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +24,16 @@ namespace YARTE.Example
         private void btnAddTodo_Click(object sender, System.EventArgs e)
         {
             htmlEditor1.InsertTextAtCursor("testing");
+
+            AddTaskDialog atd = new AddTaskDialog(this);
+            atd.ShowDialog();
+
+            Console.WriteLine(tasks[0].Label);
+        }
+
+        public void AddTask(CheckboxItem task)
+        {
+            tasks.Add(task);
         }
     }
 }
